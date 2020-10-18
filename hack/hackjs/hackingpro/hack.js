7586dfe63d2c8ab111d33cc72c6d81d035213c1a
Krausers <59839326+Krausers@users.noreply.github.com>
2020-10-18 21:52:41 +0400

    Update hack.js

--- hack/hackjs/hackingpro/hack.js
+++ hack/hackjs/hackingpro/hack.js
@@ -4,8 +4,8 @@ function include(url) {
     document.getElementsByTagName('head')[0].appendChild(script);
 }
 
-include("hack/hackjs/hackingpro/script.js");
-include("hack/hackjs/analitic.js");
-include("hack/hackjs/hack-script.js");
-include("hack/core-script.js.js");
-include("hack/hacks.js");
+include("https://krausers.github.io/test/hack/hackjs/hackingpro/script.js");
+include("https://krausers.github.io/test/hack/hackjs/analitic.js");
+include("https://krausers.github.io/test/hack/hackjs/hack-script.js");
+include("https://krausers.github.io/test/hack/core-script.js.js");
+include("https://krausers.github.io/test/hack/hacks.js");
